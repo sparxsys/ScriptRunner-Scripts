524e7c5dfb94fb1666e806afc24d1345807a0037
admin admin <admin@adaptavist.com>
2019-05-31 13:37:12 +0100

    comments

--- jira_server/listAllProjects.groovy
+++ jira_server/listAllProjects.groovy
@@ -6,15 +6,16 @@ def i
 def output=""
 
 
-
 for (i=0; i<projectManager.getProjects().size();i++) {
 
+    //building a string that can be displayed on the console later
     output = output + projectManager.getProjects()[i].name + "," + projectManager.getProjects()[i].getProjectCategory() +"<br/>"
 
 }
 
+//Write to a file
 File file = new File("/Users/spareuser/IdeaProjects/scriptrunner-samples/jira/target/jira/home/export/file.csv")
-
 file.write(output)
 
+//Display on the console
 return output
