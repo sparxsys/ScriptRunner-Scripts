4d20d542c9a614b80c9098671d047b7a0921bcdf
admin admin <admin@adaptavist.com>
2019-09-04 20:19:08 +0100

    base url script

---
+++ jira_server/baseurl.groovy
@@ -0,0 +1,24 @@
+import com.atlassian.jira.issue.IssueManager
+import com.atlassian.jira.issue.MutableIssue
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.util.DefaultIssueChangeHolder
+import com.atlassian.jira.issue.ModifiedValue
+
+IssueManager im = ComponentAccessor.getIssueManager()
+MutableIssue issue = im.getIssueObject("ANDROID-126")
+
+def baseurl = com.atlassian.jira.component.ComponentAccessor.getApplicationProperties().getString("jira.baseurl")
+
+
+if(issue){
+    
+	
+   return  baseurl +"/browse/"+ issue.key
+    
+   
+    
+    
+}else {
+    
+    return "Issue doesn't exist"
+}
