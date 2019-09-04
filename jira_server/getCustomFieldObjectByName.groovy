e2c12fdf92b72ae32930a87a85c0b4ad4e221fea
admin admin <admin@adaptavist.com>
2019-09-04 07:59:18 +0100

    New script

---
+++ jira_server/getCustomFieldObjectByName.groovy
@@ -0,0 +1,25 @@
+import com.atlassian.jira.issue.IssueManager
+import com.atlassian.jira.issue.MutableIssue
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.util.DefaultIssueChangeHolder
+import com.atlassian.jira.issue.ModifiedValue
+
+IssueManager im = ComponentAccessor.getIssueManager()
+MutableIssue issue = im.getIssueObject("ANDROID-126")
+
+if(issue){
+    
+	//get the value
+    def customFieldManager = ComponentAccessor.getCustomFieldManager()
+    //def cField = customFieldManager.getCustomFieldObject("customfield_10500")
+    
+    def cField = customFieldManager.getCustomFieldObjectByName("Text Field")
+    def cFieldValue = issue.getCustomFieldValue(cField)
+    
+   
+    
+    
+}else {
+    
+    return "Issue doesn't exist"
+}
