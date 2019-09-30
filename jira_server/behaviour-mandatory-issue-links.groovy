8309200120da1bbd60be84dc9da489e46587bbcd
Ravi Sagar <ravisagar@gmail.com>
2019-09-30 20:12:07 +0100

    Create behaviour-mandatory-issue-links.groovy

---
+++ jira_server/behaviour-mandatory-issue-links.groovy
@@ -0,0 +1,28 @@
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.link.IssueLink
+
+def links = getFieldById("issuelinks")
+def linksValue = links.getValue()
+def linkType = getFieldById("issuelinks-linktype")
+def linkTypeValue = linkType.getValue()
+def linkField = getFieldById("issuelinks-issues")
+def linkFieldValue = linkField.getValue()
+
+log.debug("Links Value: "+ linksValue)
+log.debug("Link Type Value: "+ linkTypeValue)
+log.debug("Links Value: "+ linkFieldValue)
+
+def existingLinks = ComponentAccessor.getIssueLinkManager().getInwardLinks(underlyingIssue?.getId())
+
+if (existingLinks?.size() == 0 && linkTypeValue == "is part of") {
+    
+    links.setRequired(true)
+    
+} else {
+    
+    links.setRequired(false)
+    
+}
+
+
+
