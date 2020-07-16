490ea5502e66ea543c58ef3690cc47b8fdf85507
Ravi Sagar <ravisagar@gmail.com>
2020-07-16 18:24:19 +0100

    Create getProjects.groovy

---
+++ jira_server/getProjects.groovy
@@ -0,0 +1,21 @@
+import com.atlassian.jira.component.ComponentAccessor
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+
+
+def log = Logger.getLogger("in.ravisagar.sr4j")
+log.setLevel(Level.DEBUG)
+
+def projectManager = ComponentAccessor.getProjectManager()
+
+projectManager.getProjects().each { it ->
+
+    log.debug(it.key)
+    log.debug(it.getIssueTypes()*.name)
+    log.debug(it.getComponents()*.name)
+    log.debug(it.getProjectLead()*.username)
+    log.debug(it.getVersions()*.name)
+    log.debug(it.getProjectCategoryObject()?.name)
+}
+
+
